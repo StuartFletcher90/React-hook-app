e06ae2a33c812290b9541ebab6b0a1ceeb2ec242
CN0104 <cn0104@CN11.local>
2019-11-06 08:51:20 +0000

    push from chester

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='container'>
+            <h5>About Us</h5>
+            <p>
+                Yo this is my App, there are many like it but this one is mine.
+            </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='container'>
+            <h5>Contact Us</h5>
+            <p>Yo this is my contact page, there are many like it but this one is mine!</p>
+            <div className='divider' />
+            <h6>Address:</h6>
+            <ul>
+                <li>1234 Vietnam Lane</li>
+                <li>Vietnam</li>
+                <li>Earth</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Contact;
